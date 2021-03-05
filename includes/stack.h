85239baf4562a2ee12e459e64eb900adcee91520
coscialp <corentin.scialpi@gmail.com>
2021-03-05 08:57:26 +0100

    stack.push update

--- includes/stack.h
+++ includes/stack.h
@@ -6,7 +6,7 @@
 /*   By: coscialp <coscialp@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/04 15:02:25 by coscialp          #+#    #+#             */
-/*   Updated: 2021/03/04 15:24:11 by coscialp         ###   ########lyon.fr   */
+/*   Updated: 2021/03/05 08:56:48 by coscialp         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,14 +28,14 @@ struct s_stack
 {
 	t_node_stack	*_data;
 	size_t			_size;
-	void			(*push)(t_stack *, int);
+	int				(*push)(t_stack *, int);
 	void			(*pop)(t_stack *);
 	size_t			(*size)(t_stack *);
 };
 
 t_stack	new_stack(void);
 size_t	stack_size(t_stack *stack);
-void	stack_push(t_stack *stack, int data);
+int		stack_push(t_stack *stack, int data);
 void	stack_pop(t_stack *stack);
 
 #endif
--- src/stack/stack_push.c
+++ src/stack/stack_push.c
@@ -6,18 +6,24 @@
 /*   By: coscialp <coscialp@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/04 15:24:31 by coscialp          #+#    #+#             */
-/*   Updated: 2021/03/04 15:29:43 by coscialp         ###   ########lyon.fr   */
+/*   Updated: 2021/03/05 08:44:52 by coscialp         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "stack.h"
 
-void	stack_push(t_stack *stack, int data)
+int	stack_push(t_stack *stack, int data)
 {
 	t_node_stack	*new_node;
+	t_node_stack	*tmp;
 
-	if (!stack)
-		return ;
+	tmp = stack->_data;
+	while (tmp)
+	{
+		if (tmp->value == data)
+			return (-1);
+		tmp = tmp->_next;
+	}
 	new_node = ft_xmalloc(sizeof(t_node_stack));
 	new_node->value = data;
 	new_node->_next = NULL;
@@ -31,4 +37,5 @@ void	stack_push(t_stack *stack, int data)
 		stack->_data = new_node;
 	}
 	stack->_size++;
+	return (0);
 }
